5c6e5630576a1a66f0daeee1ac10f8e406d5ec5c
kansasSamurai <kansasSamurai@users.noreply.github.com>
2019-11-08 17:40:22 -0600

    Create SmoothLabel.java

---
+++ src/main/java/org/jwellman/filedropper/SmoothLabel.java
@@ -0,0 +1,27 @@
+package org.jwellman.filedropper;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import javax.swing.JLabel;
+
+@SuppressWarnings("serial")
+public class SmoothLabel extends JLabel {
+
+	public SmoothLabel() {
+		super();
+	}
+	
+    public SmoothLabel(String text) {
+        super(text);
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        super.paintComponent(g2d);
+    }
+    
+}
